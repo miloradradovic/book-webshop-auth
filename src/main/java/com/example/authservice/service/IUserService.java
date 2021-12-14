50cb9ce91b2d77fabcfbeb90e28c31b24c4bce9b
miloradradovic <radovic.milorad1998@gmail.com>
2021-12-14 11:22:24 +0100

    auth setup

--- src/main/java/com/example/authservice/service/IUserService.java
+++ src/main/java/com/example/authservice/service/IUserService.java
@@ -5,4 +5,5 @@ import com.example.authservice.model.User;
 public interface IUserService {
 
     User findByUsernameAndPassword(String username, String password);
+    User findByUsername(String username);
 }
--- src/main/java/com/example/authservice/service/impl/UserService.java
+++ src/main/java/com/example/authservice/service/impl/UserService.java
@@ -16,4 +16,9 @@ public class UserService implements IUserService {
     public User findByUsernameAndPassword(String username, String password) {
         return userRepository.findByUsernameAndPassword(username, password);
     }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
